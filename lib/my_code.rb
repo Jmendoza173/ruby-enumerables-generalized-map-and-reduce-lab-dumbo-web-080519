81fcc673311fc6ab5a84ebf6b7d48bdb75b64a25
Joel Mendoza <joelmendza173@gmail.com>
2019-07-28 01:20:46 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,7 +3,7 @@ def map(source_array)
   new = []
   i = 0
   while i < source_array.length do
-    new.push( source_array[i] ) # <== Unique work
+    newary << yield
     i += 1
   end
   return new
