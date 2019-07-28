eb9630ffb3452dd39df819e7d82542d21a3809c2
Joel Mendoza <joelmendza173@gmail.com>
2019-07-28 01:19:01 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,6 +1,12 @@
 # Your Code Here
-def map(array, blok)
-  yield(array.map{blok})
+def map(source_array)
+  new = []
+  i = 0
+  while i < source_array.length do
+    new.push( source_array[i] ) # <== Unique work
+    i += 1
+  end
+  return new
 end
 
 map(array_source, ){|inner|
